773861918f025623c432d0b52a6cca40a94abd91
Khasbulat Abdullin <khas.2015@gmail.com>
2023-03-01 18:25:00 +0300

    deploy

---
+++ server/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const public = path.join(path.resolve(__dirname, '..'), 'public');
+
+const config = {
+    PORT: 8080,
+    HOST: "localhost"
+}
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(public, 'index.html'));
+});
+
+app.use('/', express.static(public));
+
+app.listen(config.PORT, config.HOST);
