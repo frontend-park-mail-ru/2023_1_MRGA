92dbf9fc080c2cf64be82061122221d4897115ac
Andrey <84812632+TonL@users.noreply.github.com>
2023-04-25 21:37:22 +0300

    сверстал главную страницу (about page)

--- public/components/App/pages/about/aboutPage.js
+++ public/components/App/pages/about/aboutPage.js
@@ -1,14 +1,40 @@
 import {Header} from "components/App/header/header";
 import {FormContainer} from "components/UI/containers/formContainer/formContainer";
-
+import logo from "assets/Logo.svg"
+import inLove from "assets/svg/inLove.svg"
+import styles from './aboutPage.module.css'
 export const AboutPage = () => {
     return (
         <>
-            <Header></Header>
-            <FormContainer>
-                <div>Всем привет. Это About Page</div>
-            </FormContainer>
+            <div className={styles.page}>
+                <img className={styles.test} src={logo} width="203" alt={logo}/>
 
+                <div className={styles.container}>
+                    <div className={styles.left}>
+                        <div className={styles.slogan}>
+                            <div>Знакомьтесь</div>
+                            <div>Слушайте</div>
+                            <div>Общайтесь</div>
+                        </div>
+                        <a href="#">
+                            <div className={styles.regBtn}>
+                                Регистрация
+                            </div>
+                        </a>
+                            <div className={styles.loginInvite}>
+                                Уже есть аккаунт?
+
+                                <a href="#" className={styles.loginBtn}>
+                                    Войти
+                                </a>
+
+                            </div>
+                    </div>
+                    <div className={styles.right}>
+                        <img src={inLove} alt=""/>
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
