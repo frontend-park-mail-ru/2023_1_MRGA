acd1188492609cd1a47a5c727b5d9f8e1c1a52ff
Khasbulat Abdullin <khas.2015@gmail.com>
2023-04-25 20:38:38 +0300

    в компоненте navigate не происходит ничего, если путь не изменился

--- public/lib/jsx/components/navigate/navigate.js
+++ public/lib/jsx/components/navigate/navigate.js
@@ -1,4 +1,8 @@
 export const Navigate = ({to}) => {
+    const currentPath = window.location.pathname;
+    if (currentPath === to) {
+        return ;
+    }
     window.history.pushState({}, to,  to);
     const popStateEvent = new PopStateEvent("popstate", { state: history.state });
     window.dispatchEvent(popStateEvent);
