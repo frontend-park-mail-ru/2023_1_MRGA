19164e5c2ab7eaffe2a76e97d63428155ee0c56e
Khasbulat Abdullin <khas.2015@gmail.com>
2023-04-25 20:32:13 +0300

    отмена перерендера, если пути совпадают в компоненте Link

--- public/lib/jsx/components/link/link.js
+++ public/lib/jsx/components/link/link.js
@@ -3,6 +3,10 @@ import {Navigate} from "@/lib/jsx/components/navigate/navigate";
 export const Link = ({href, ...props}, children) => {
     const linkClickHandler = (event) => {
         event.preventDefault();
+        const currentPath = window.location.pathname;
+        if (currentPath === href) {
+            return ;
+        }
         Navigate({to:href});
     }
     return (
