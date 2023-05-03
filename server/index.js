6f6d40522d7c5e9c0789cc896725dff0a887dd55
masharpik <maria.naberezhnya@gmail.com>
2023-05-03 03:41:47 +0300

    Написала сервер ws который регистрирует, пересылает сообщения и удаляет пользователей

--- server/index.js
+++ server/index.js
@@ -1,13 +1,22 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const ws = require('ws');
+const ws_connection = require('./ws');
+
 const public = path.join(path.resolve(__dirname, '..'), 'dist');
 
 const config = {
     PORT: 3000,
+    WS_PORT: 9090,
     // HOST: "0.0.0.0"
     HOST: "192.168.0.45"
 }
+
+const wsServer = new WebSocketServer.Server({host: config.HOST, port: WS_PORT}, () => {
+    console.log(`WebSocketServer is running on ${config.HOST}:${config.WS_PORT}`);
+});
+wsServer.on('connection', ws_connection);
 
 app.get('/', function(req, res) {
     res.sendFile(path.join(public, 'index.html'));
---
+++ server/ws.js
@@ -0,0 +1,105 @@
+let userIdClients = new Map();
+let id = 1;
+
+module.exports = function onConnect(wsClient) {
+    wsClient.send(JSON.stringify({
+        flag: "CONN+ASK",
+        status: 200,
+    }));
+    
+    wsClient.on('message', function(message) {
+
+        const jsonMSG = JSON.parse(message);
+
+        switch (jsonMSG.flag) {
+        case "REG":
+            const userId = jsonMSG.body.userId;
+            wsClient.userId = userId;
+            wsClient.id = id;
+            id++;
+        
+            console.log('В запросе REG от %d получены данные: %s', wsClient.userId, jsonMSG);
+
+            let clientsByUser = userIdClients.get(userId);
+
+            if (clientsByUser === undefined || clientsByUser.length === 0) {
+                userIdClients.set(userId, [wsClient]);
+            }
+            if (clientsByUser.length > 0) {
+                clientsByUser.push(wsClient);
+                userIdClients.set(userId, clientsByUser);
+            } else {
+                wsClient.send(JSON.stringify({
+                    flag: "REG+ASK",
+                    status: 404,
+                    err: `Не удается зарегистрировать пользователя ${userId} на сервере.`,
+                }));
+                return;
+            }
+
+            wsClient.send(JSON.stringify({
+                flag: "REG+ASK",
+                status: 200,
+            }));
+
+            break;
+        case "SEND":
+            console.log('В запросе SEND от %d получены данные: %s', wsClient.userId, jsonMSG);
+
+            const msg = jsonMSG.body.msg;
+            const userIds = jsonMSG.body.userIds;
+            const chatId = jsonMSG.body.chatId;
+
+            userIds.forEach(userId => {
+                const clientsByUser = userIdClients.get(userId);
+                if (clientsByUser === undefined || clientsByUser.length === 0) {
+                    return;
+                }
+                if (clientsByUser.length > 0) {
+                    clientsByUser.forEach(receiverWsClient => {
+                        if (receiverWsClient === undefined) {
+                            return;
+                        }
+                        receiverWsClient.send(
+                            JSON.stringify({
+                                flag: "SEND",
+                                body: {
+                                    chatId: chatId,
+                                    senderId: wsClient.userId,
+                                    msg: msg,
+                                    sendAt: getNowTime(),
+                                },
+                            })
+                        );
+                    });
+                } else {
+                    wsClient.send(JSON.stringify({
+                        flag: "SEND+ASK",
+                        status: 500,
+                        err: `Не удалось отправить сообщение '${msg}' пользователю ${userId}`,
+                    }));
+                    return;
+                }
+            });
+            break;
+        }
+    });
+    
+    wsClient.on('close', () => {
+        let clientsByUser = userIdClients.get(wsClient.userId);
+        userIdClients.set(userId, clientsByUser.filter(client => client.id != wsClient.id));
+        console.log('Соединение с клиентом %d закрыто', wsClient.id);
+    });
+};
+
+function getNowTime() {
+    let now = new Date();
+    let hours = now.getHours().toString().padStart(2, '0');
+    let minutes = now.getMinutes().toString().padStart(2, '0');
+    let day = now.getDate().toString().padStart(2, '0');
+    let month = (now.getMonth() + 1).toString().padStart(2, '0');
+    let year = now.getFullYear().toString();
+    let formattedDate = `${hours}:${minutes} ${day}.${month}.${year}`;
+    
+    return formattedDate;
+}
